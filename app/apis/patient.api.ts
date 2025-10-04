800ab90e39f5197c2b153aae01392fbb3674f168
anhsayshello <anhle0347@gmail.com>
2025-10-04 12:35:11 +0700

    refactor: update type defs

--- app/apis/patient.api.ts
+++ app/apis/patient.api.ts
@@ -1,10 +1,21 @@
+import type { Appointment, AppointmentsChart, AppointmentStatusCount } from '@/types/appointment.type'
+import type { Doctor } from '@/types/doctor.type'
 import type { Patient } from '@/types/patient.type'
 import type { Role } from '@/types/role.type'
 import http from '@/utils/http'
 
 const patientApi = {
-  getPatientInformation: () => http.get<{ user: Patient; role: Role }>('patient/information'),
-  upsertPatient: (body: Omit<Patient, 'uid'>) => http.post<{ user: Patient; role: Role }>('patient/upsert', body)
+  getPatientInformation: () => http.get<{ data: Patient }>('patient/information'),
+  getPatientDashboardStatistic: () =>
+    http.get<{
+      data: Patient
+      appointmentCounts: AppointmentStatusCount
+      last5Records: Appointment[]
+      totalAppointments: number
+      availabelDoctor: Doctor
+      monthlyData: AppointmentsChart
+    }>('patient/statistic'),
+  upsertPatient: (body: Omit<Patient, 'uid'>) => http.post<{ data: Patient; role: Role }>('patient/upsert', body)
 }
 
 export default patientApi
