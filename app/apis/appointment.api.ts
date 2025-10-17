284521c3af4c171464b5d92e834aae1bd04e2db0
anhsayshello <anhle0347@gmail.com>
2025-10-17 17:10:46 +0700

    feat(api): add update appointment detail endpoint

--- app/apis/appointment.api.ts
+++ app/apis/appointment.api.ts
@@ -1,8 +1,10 @@
-import type { Appointment } from '@/types/appointment.type'
+import type { Appointment, AppointmentStatus } from '@/types/appointment.type'
 import http from '@/utils/http'
 
 const appointmentApi = {
-  getAppointmentDetail: (id: string) => http.get<{ data: Appointment }>(`appointment/${id}`)
+  getAppointmentDetail: (id: string) => http.get<{ data: Appointment }>(`appointment/${id}`),
+  updateAppointmentDetail: ({ id, status, reason }: { id: string; status: AppointmentStatus; reason: string }) =>
+    http.post<{ data: Appointment }>(`appointment/${id}`, { status, reason })
 }
 
 export default appointmentApi
