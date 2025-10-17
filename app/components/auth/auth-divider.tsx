4086c5e02f1fbb79829668004b7152e9f74a0cce
anhsayshello <anhle0347@gmail.com>
2025-10-17 17:13:16 +0700

    styles: change compatible background

--- app/components/auth/auth-divider.tsx
+++ app/components/auth/auth-divider.tsx
@@ -1,7 +1,7 @@
 export default function AuthDivider() {
   return (
     <div className="flex items-center justify-center before:content-[''] before:flex-1 before:h-px before:bg-primary/10 after:content-[''] after:flex-1 after:h-px after:bg-primary/10">
-      <span className='text-xs text-muted-foreground font-medium px-5 bg-white uppercase'>or continue with</span>
+      <span className='text-xs text-muted-foreground font-medium px-5 bg-background uppercase'>or continue with</span>
     </div>
   )
 }
