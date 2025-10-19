769ce6425043a36e286d7b464dac19a4fbc5eb54
anhsayshello <anhle0347@gmail.com>
2025-10-19 10:23:12 +0700

    chore: unused features

--- app/hooks/use-admin.ts
+++
@@ -1,9 +0,0 @@
-import { useAuthStore } from '@/stores/useAuthStore'
-import { RoleEnum } from '@/types/role.type'
-
-export default function useIsAdmin() {
-  const role = useAuthStore((state) => state.role)
-  const isAdmin = role === RoleEnum.ADMIN
-
-  return isAdmin
-}
