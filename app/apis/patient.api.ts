e9e651f8914936439db1748e437045b5e4573d30
anhsayshello <anhle0347@gmail.com>
2025-10-20 10:27:41 +0700

    refactor: omit created at, updated at params

--- app/apis/patient.api.ts
+++ app/apis/patient.api.ts
@@ -8,7 +8,8 @@ const patientApi = {
   getPatientDashboardStatistic: () => http.get<PatientDashboardStatistic>('patient/statistic'),
   getPatientAppointment: (params: SearchQueryParams) =>
     http.get<PatientAppointment>('patient/appointments', { params }),
-  upsertPatient: (body: Omit<Patient, 'uid'>) => http.post<{ data: Patient; role: Role }>('patient/upsert', body)
+  upsertPatient: (body: Omit<Patient, 'uid' | 'created_at' | 'updated_at'>) =>
+    http.post<{ data: Patient; role: Role }>('patient/upsert', body)
 }
 
 export default patientApi
