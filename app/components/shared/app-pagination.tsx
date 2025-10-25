20a41123a694122c5b0a7dfccfc256cc10390f48
anhsayshello <anhle0347@gmail.com>
2025-10-25 11:07:21 +0700

    chore: extract pagnitaion to another file

---
+++ app/components/shared/app-pagination.tsx
@@ -0,0 +1,107 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious
+} from '@/components/ui/pagination'
+import { useCallback, useMemo } from 'react'
+
+export default function AppPagination({
+  currentPage,
+  totalPages,
+  handlePageChange
+}: {
+  currentPage: number
+  totalPages: number
+  handlePageChange: (page: number) => void
+}) {
+  const generatePageNumbers = useCallback((currentPage: number, totalPages: number) => {
+    const pages = []
+    const maxvisiblePages = 3
+
+    if (totalPages <= maxvisiblePages) {
+      for (let i = 1; i <= totalPages; i++) {
+        pages.push(i)
+      }
+    } else {
+      if (currentPage <= 2) {
+        pages.push(1, 2, 3)
+      } else if (currentPage >= totalPages - 1) {
+        pages.push(totalPages - 2, totalPages - 1, totalPages)
+      } else {
+        pages.push(currentPage - 1, currentPage, currentPage + 1)
+      }
+    }
+    return pages
+  }, [])
+
+  const pageNumbers = useMemo(
+    () => generatePageNumbers(currentPage, totalPages),
+    [currentPage, totalPages, generatePageNumbers]
+  )
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            onClick={() => handlePageChange(currentPage - 1)}
+            className={currentPage === 1 ? 'pointer-events-none opacity-50' : 'cursor-pointer'}
+          />
+        </PaginationItem>
+
+        {pageNumbers[0] > 1 && (
+          <>
+            <PaginationItem>
+              <PaginationLink onClick={() => handlePageChange(1)} className='cursor-pointer'>
+                1
+              </PaginationLink>
+            </PaginationItem>
+            {pageNumbers[0] > 2 && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+          </>
+        )}
+
+        {pageNumbers.map((pageNum) => (
+          <PaginationItem key={pageNum}>
+            <PaginationLink
+              onClick={() => handlePageChange(pageNum)}
+              isActive={pageNum === currentPage}
+              className='cursor-pointer'
+            >
+              {pageNum}
+            </PaginationLink>
+          </PaginationItem>
+        ))}
+
+        {pageNumbers[pageNumbers.length - 1] < totalPages && (
+          <>
+            {pageNumbers[pageNumbers.length - 1] < totalPages - 1 && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+            <PaginationItem>
+              <PaginationLink onClick={() => handlePageChange(totalPages)} className='cursor-pointer'>
+                {totalPages}
+              </PaginationLink>
+            </PaginationItem>
+          </>
+        )}
+
+        <PaginationItem>
+          <PaginationNext
+            onClick={() => handlePageChange(currentPage + 1)}
+            className={currentPage === totalPages ? 'pointer-events-none opacity-50' : 'cursor-pointer'}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  )
+}
--- app/routes/appointments.tsx
+++ app/routes/appointments.tsx
@@ -1,14 +1,5 @@
 import { useSearchParams } from 'react-router'
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious
-} from '@/components/ui/pagination'
-import { useCallback, useMemo } from 'react'
+
 import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import patientApi from '@/apis/patient.api'
 import AppointmentRecords from '@/components/appointments/appointment-records'
@@ -16,6 +7,8 @@ import { omitBy, isUndefined } from 'lodash'
 import { useAuthStore } from '@/stores/useAuthStore'
 import doctorApi from '@/apis/doctor.api'
 import useRole from '@/hooks/use-role'
+import { useMemo } from 'react'
+import AppPagination from '@/components/shared/app-pagination'
 
 export default function Appointments() {
   const user = useAuthStore((state) => state.user)
@@ -57,31 +50,6 @@ export default function Appointments() {
     [dataPatientAppointments, dataDoctorAppointments]
   )
 
-  const generatePageNumbers = useCallback((currentPage: number, totalPages: number) => {
-    const pages = []
-    const maxvisiblePages = 3
-
-    if (totalPages <= maxvisiblePages) {
-      for (let i = 1; i <= totalPages; i++) {
-        pages.push(i)
-      }
-    } else {
-      if (currentPage <= 2) {
-        pages.push(1, 2, 3)
-      } else if (currentPage >= totalPages - 1) {
-        pages.push(totalPages - 2, totalPages - 1, totalPages)
-      } else {
-        pages.push(currentPage - 1, currentPage, currentPage + 1)
-      }
-    }
-    return pages
-  }, [])
-
-  const pageNumbers = useMemo(
-    () => generatePageNumbers(currentPage, totalPages),
-    [currentPage, totalPages, generatePageNumbers]
-  )
-
   const handlePageChange = (newPage: number) => {
     setSearchParams({ page: String(newPage) })
   }
@@ -95,88 +63,9 @@ export default function Appointments() {
       {dataAppoinments && (
         <>
           <AppointmentRecords onSearch={handleSearch} data={dataAppoinments} totalRecords={totalRecords} />
-          <AppointmentsPagination
-            handlePageChange={handlePageChange}
-            pageNumbers={pageNumbers}
-            currentPage={currentPage}
-            totalPages={totalPages}
-          />
+          <AppPagination handlePageChange={handlePageChange} currentPage={currentPage} totalPages={totalPages} />
         </>
       )}
     </div>
-  )
-}
-
-function AppointmentsPagination({
-  currentPage,
-  totalPages,
-  handlePageChange,
-  pageNumbers
-}: {
-  currentPage: number
-  totalPages: number
-  handlePageChange: (page: number) => void
-  pageNumbers: number[]
-}) {
-  return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious
-            onClick={() => handlePageChange(currentPage - 1)}
-            className={currentPage === 1 ? 'pointer-events-none opacity-50' : 'cursor-pointer'}
-          />
-        </PaginationItem>
-
-        {pageNumbers[0] > 1 && (
-          <>
-            <PaginationItem>
-              <PaginationLink onClick={() => handlePageChange(1)} className='cursor-pointer'>
-                1
-              </PaginationLink>
-            </PaginationItem>
-            {pageNumbers[0] > 2 && (
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-            )}
-          </>
-        )}
-
-        {pageNumbers.map((pageNum) => (
-          <PaginationItem key={pageNum}>
-            <PaginationLink
-              onClick={() => handlePageChange(pageNum)}
-              isActive={pageNum === currentPage}
-              className='cursor-pointer'
-            >
-              {pageNum}
-            </PaginationLink>
-          </PaginationItem>
-        ))}
-
-        {pageNumbers[pageNumbers.length - 1] < totalPages && (
-          <>
-            {pageNumbers[pageNumbers.length - 1] < totalPages - 1 && (
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-            )}
-            <PaginationItem>
-              <PaginationLink onClick={() => handlePageChange(totalPages)} className='cursor-pointer'>
-                {totalPages}
-              </PaginationLink>
-            </PaginationItem>
-          </>
-        )}
-
-        <PaginationItem>
-          <PaginationNext
-            onClick={() => handlePageChange(currentPage + 1)}
-            className={currentPage === totalPages ? 'pointer-events-none opacity-50' : 'cursor-pointer'}
-          />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
   )
 }
