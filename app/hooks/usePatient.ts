1c6e23b1444a1f89a625252bace0110929554f27
anhsayshello <anhle0347@gmail.com>
2025-11-03 10:27:00 +0700

    feat: add new query hooks

---
+++ app/hooks/usePatient.ts
@@ -0,0 +1,14 @@
+import patientApi from '@/apis/patient.api'
+import { useQuery } from '@tanstack/react-query'
+
+export default function usePatient(uid: string) {
+  const { data, isPending } = useQuery({
+    queryKey: ['patient', uid],
+    queryFn: () => patientApi.getPatientById(uid),
+    enabled: !!uid
+  })
+
+  const dataPatient = data?.data.data
+
+  return { dataPatient, isPending }
+}
