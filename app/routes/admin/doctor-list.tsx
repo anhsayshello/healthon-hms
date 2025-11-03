096fdbd35cf8492be0ddbaf1228ff8b2469b7976
anhsayshello <anhle0347@gmail.com>
2025-11-03 10:31:42 +0700

    feat: new page

--- app/routes/admin/doctor-list.tsx
+++ app/routes/admin/doctor-list.tsx
@@ -9,6 +9,12 @@ import TableMetadata from '@/components/shared/table-metadata'
 import AppPagination from '@/components/shared/app-pagination'
 import NewDoctor from './new-doctor'
 import UserAction from './user-action'
+import type { Doctor } from '@/types/doctor.type'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import UserDetailsDialog from './user-details-dialog'
+import { RoleEnum } from '@/types/role.type'
+import InfoItem from '@/components/shared/info-item'
+import { Badge } from '@/components/ui/badge'
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Doctor Table' }, { name: 'description', content: 'Welcome to React Router!' }]
@@ -47,29 +53,57 @@ export default function DoctorList() {
           <TableBody>
             {dataDoctors &&
               dataDoctors.length > 0 &&
-              dataDoctors.map((doctor) => (
-                <TableRow key={doctor.uid}>
-                  <TableCell>
-                    <UserInfo
-                      photoUrl={doctor.photo_url}
-                      firstName={doctor.first_name}
-                      lastName={doctor.last_name}
-                      description={doctor.specialization}
-                    />
-                  </TableCell>
-                  <TableCell>{doctor.email}</TableCell>
-                  <TableCell>{doctor.phone}</TableCell>
-                  <TableCell>{doctor.license_number}</TableCell>
-                  <TableCell>{doctor.address}</TableCell>
-                  <TableCell>
-                    <UserAction uid={doctor.uid} email={doctor.email} />
-                  </TableCell>
-                </TableRow>
-              ))}
+              dataDoctors.map((doctor) => <DoctorTableRow key={doctor.uid} doctor={doctor} />)}
           </TableBody>
         </Table>
       </CardWrapper>
       <AppPagination handlePageChange={handlePageChange} currentPage={currentPage} totalPages={totalPages} />
     </div>
+  )
+}
+
+function DoctorTableRow({ doctor }: { doctor: Doctor }) {
+  return (
+    <Dialog>
+      <TableRow>
+        <DialogTrigger asChild>
+          <TableCell className='cursor-pointer'>
+            <UserInfo
+              photoUrl={doctor.photo_url}
+              firstName={doctor.first_name}
+              lastName={doctor.last_name}
+              description={doctor.specialization}
+            />
+          </TableCell>
+        </DialogTrigger>
+        <TableCell>{doctor.email}</TableCell>
+        <TableCell>{doctor.phone}</TableCell>
+        <TableCell>{doctor.license_number}</TableCell>
+        <TableCell>{doctor.address}</TableCell>
+        <TableCell>
+          <UserAction uid={doctor.uid} email={doctor.email} />
+        </TableCell>
+      </TableRow>
+      <UserDetailsDialog user={doctor} role={RoleEnum.DOCTOR}>
+        <div className='grid grid-cols-2 gap-4'>
+          <InfoItem label='Phone' value={doctor?.phone} />
+          <InfoItem label='Address' value={doctor?.address} />
+          <InfoItem label='License Number' value={doctor?.license_number} />
+          <InfoItem label='Specialization' value={doctor?.specialization} />
+          <InfoItem label='Department' value={doctor?.department || 'N/A'} />
+          <InfoItem label='Status' value={doctor?.availability_status || 'N/A'} />
+        </div>
+        {doctor.working_days && (
+          <div>
+            <h4 className='font-semibold mb-3 text-lg'>Working Schedule</h4>
+            <div className='space-y-2 space-x-2'>
+              {doctor?.working_days?.map((item) => (
+                <Badge key={item.id}>{item.day}</Badge>
+              ))}
+            </div>
+          </div>
+        )}
+      </UserDetailsDialog>
+    </Dialog>
   )
 }
