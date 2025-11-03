3f96c961885b571a81507d52d3ce58111eb74edc
anhsayshello <anhle0347@gmail.com>
2025-11-03 10:25:50 +0700

    styles: simplify stat card ui

--- app/components/appointments/stat-card.tsx
+++ app/components/appointments/stat-card.tsx
@@ -1,9 +1,6 @@
 import type { LucideIcon } from 'lucide-react'
 import { Card, CardContent, CardFooter, CardHeader } from '../ui/card'
 import { cn } from '@/lib/utils'
-import { Button } from '../ui/button'
-import { Link } from 'react-router'
-import formatNumber from '@/helpers/formatNumber'
 
 interface Props {
   title: string
@@ -12,43 +9,23 @@ interface Props {
   className?: string
   iconClassName?: string
   note: string
-  link: string
 }
 
 const CardIcon = ({ icon: Icon }: { icon: LucideIcon }) => {
-  return <Icon />
+  return <Icon size={20} />
 }
 
-export default function StatCard({ title, value, icon, className, iconClassName, note, link }: Props) {
+export default function StatCard({ title, value, icon, className }: Props) {
   return (
-    <Card className={cn('w-full md:w-70 2xl:w-80', className)}>
-      <CardHeader className='flex items-center justify-between pb-1 capitalize'>
-        <h3 className='md:text-lg font-semibold'>{title}</h3>
-        <Button
-          variant='outline'
-          asChild
-          size='sm'
-          className='font-normal text-xs bg-transparent p-2.5 h-0 hover:underline'
-        >
-          <Link to={link}> See details</Link>
-        </Button>
+    <Card className={cn('w-full', className)}>
+      <CardHeader className='flex items-center justify-between capitalize'>
+        <div className='tracking-tight font-medium text-sm'>{title}</div>
+        <CardIcon icon={icon} />
       </CardHeader>
       <CardContent>
-        <div className='flex items-center gap-4'>
-          <div
-            className={cn(
-              'w-10 h-10 bg-violet-50/500/15 rounded-full flex items-center justify-center text-violet-600',
-              iconClassName
-            )}
-          >
-            <CardIcon icon={icon} />
-          </div>
-          <h2 className='text-xl 2xl:text-2xl font-semibold'>{formatNumber(value)}</h2>
-        </div>
+        <div className='text-2xl font-semibold'>{value}</div>
       </CardContent>
-      <CardFooter className='px-6'>
-        <p className='text-sm text-muted-foreground capitalize'>{note}</p>
-      </CardFooter>
+      <CardFooter></CardFooter>
     </Card>
   )
 }
