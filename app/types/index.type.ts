a69ef78e3cb6ca38af2760f1653aeee57bc1367a
anhsayshello <anhle0347@gmail.com>
2025-11-03 10:34:26 +0700

    feat: add new types

--- app/types/index.type.ts
+++ app/types/index.type.ts
@@ -1,5 +1,8 @@
 import type { UserCredential } from 'firebase/auth'
 import type { Role } from './role.type'
+import type { Appointment } from './appointment.type'
+import type { Patient } from './patient.type'
+import type { Doctor } from './doctor.type'
 
 export interface GoogleTokenResponse {
   federatedId: string
@@ -64,4 +67,125 @@ export interface PaginatedResponse<Data> {
   totalPages: number
   currentPage: number
   totalRecords: number
+}
+
+type PaymentMethod = 'CASH' | 'CARD'
+
+type PaymentStatus = 'PAID' | 'UNPAID' | 'PART'
+
+export interface Payment {
+  id: number
+  bill_id?: number
+  patient_id: string
+  appointment_id: number
+  bill_date: string
+  payment_date: string
+  discount: number
+  total_amount: number
+  amount_paid: number
+  payment_method: PaymentMethod
+  status: PaymentStatus
+  receipt_number: number
+  created_at: string
+  updated_at: string
+
+  appointment?: Appointment
+  patient?: Patient
+  bills?: PatientBills[]
+}
+
+export interface PatientBills {
+  id: number
+  bill_id: number
+  service_id: number
+  service_date: string
+  quantity: number
+  unit_cost: number
+  total_cost: number
+  created_at: string
+  updated_at: string
+
+  service?: Services
+  payment?: Payment
+}
+
+export interface LabTest {
+  id: number
+  record_id: number
+  test_date: string
+  result: string
+  status: string
+  notes: string
+  service_id: number
+  created_at: string
+  updated_at: string
+
+  services?: Services
+  medical_record?: MedicalRecords
+}
+
+export interface MedicalRecords {
+  id: number
+  patient_id: string
+  appointment_id: number
+  doctor_id: string
+  treatment_plan?: string
+  prescriptions?: string
+  lab_request?: string
+  notes?: string
+  created_at: string
+  updated_at: string
+
+  appointment?: Appointment
+  patient?: Patient
+  lab_test?: LabTest[]
+  vital_signs?: VitalSigns[]
+  diagnosis?: Diagnosis[]
+}
+
+export interface VitalSigns {
+  id: number
+  patient_id: string
+  medical_id: number
+  body_temperature: number
+  systolic: number
+  diastolic: number
+  heartRate: string
+  respiratory_rate?: number
+  oxygen_saturaion?: number
+  weight: number
+  height: number
+  created_at: string
+  updated_at: string
+
+  medical_record?: MedicalRecords
+}
+
+export interface Diagnosis {
+  id: number
+  patient_id: string
+  medical_id: number
+  doctor_id: string
+  symptoms: string
+  diagnosis: string
+  notes?: string
+  prescribed_medications?: string
+  follow_up_plan?: string
+  created_at: string
+  updated_at: string
+
+  doctor?: Doctor
+  medical?: MedicalRecords
+}
+
+export interface Services {
+  id: number
+  service_name: string
+  description: string
+  price: number
+  created_at: string
+  updated_at: string
+
+  labtest?: LabTest
+  bills?: PatientBills[]
 }
