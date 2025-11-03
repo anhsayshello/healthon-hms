84482d8838a1d0019a3e64151f1bcbf20cd85d36
anhsayshello <anhle0347@gmail.com>
2025-11-03 10:24:32 +0700

    feat: add getDoctorById api

--- app/apis/doctor.api.ts
+++ app/apis/doctor.api.ts
@@ -7,7 +7,8 @@ const doctorApi = {
   getDoctors: (params: SearchQueryParams) => http.get<PaginatedResponse<Doctor>>('doctor', { params }),
   getDoctorAppointments: (params: SearchQueryParams) =>
     http.get<PaginatedResponse<Appointment>>('doctor/appointments', { params }),
-  getDoctorDashboardStatistic: () => http.get<DoctorDashboardStatistic>('doctor/statistic')
+  getDoctorDashboardStatistic: () => http.get<DoctorDashboardStatistic>('doctor/statistic'),
+  getDoctorById: (uid: string) => http.get(`doctor/${uid}`)
 }
 
 export default doctorApi
