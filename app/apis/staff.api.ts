74c08e50012d8d80503950c2e141ec9ec4d00bb7
anhsayshello <anhle0347@gmail.com>
2025-11-05 09:46:18 +0700

    feat: add staff dashboard

--- app/apis/staff.api.ts
+++ app/apis/staff.api.ts
@@ -1,8 +1,9 @@
 import type { PaginatedResponse, SearchQueryParams } from '@/types/index.type'
-import type { Staff } from '@/types/staff.type'
+import type { Staff, StaffDashboardStatistic } from '@/types/staff.type'
 import http from '@/utils/http'
 
 const staffApi = {
+  getStaffDashboardStatistic: () => http.get<StaffDashboardStatistic>('staff/statistic'),
   getStaffs: (params: SearchQueryParams) => http.get<PaginatedResponse<Staff>>('staff', { params }),
   getStaffById: (uid: string) => http.get<Staff>(`staff/${uid}`)
 }
