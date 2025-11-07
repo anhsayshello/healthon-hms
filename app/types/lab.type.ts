9a2dc6b34c6a3b3a2fe7cb63425b81cb9088529d
anhsayshello <anhle0347@gmail.com>
2025-11-07 10:20:49 +0700

    ✨ feat(types): add lab and service related types

---
+++ app/types/lab.type.ts
@@ -0,0 +1,50 @@
+import type { MedicalRecord, Payment } from './medical.type'
+import type { Staff } from './staff.type'
+
+export type LabTestStatus = 'PENDING' | 'IN_PROGRESS' | 'COMPLETED' | 'CANCELLED'
+export type Currency = 'VND' | 'USD' | 'EUR'
+
+export interface Service {
+  id: number
+  service_name: string
+  description: string
+  price: number
+  currency: Currency
+  created_at: string
+  updated_at: string
+
+  lab_test?: LabTest[]
+  lab_bills?: LabBill[]
+}
+
+export interface LabTest {
+  id: number
+  service_id: number
+  medical_id: number
+  technician_id?: string
+  test_date: string
+  result?: string
+  status: LabTestStatus
+  notes: string
+  created_at: string
+  updated_at: string
+
+  technician?: Staff
+  service?: Service
+  medical_record?: MedicalRecord
+}
+
+export interface LabBill {
+  id: number
+  bill_id: number
+  service_id: number
+  service_date: string
+  quantity: number
+  unit_cost: number
+  total_cost: number
+  created_at: string
+  updated_at: string
+
+  service?: Service
+  payment?: Payment
+}
