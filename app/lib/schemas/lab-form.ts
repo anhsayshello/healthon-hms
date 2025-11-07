409bca5716497a89350aa26d6c6fe9ff89ae6e6c
anhsayshello <anhle0347@gmail.com>
2025-11-07 10:17:30 +0700

    ✨ feat(schemas): add lab service and request form schemas

---
+++ app/lib/schemas/lab-form.ts
@@ -0,0 +1,12 @@
+import z from 'zod'
+
+export const LabServiceForm = z.object({
+  service_name: z.string().max(200, 'Service name must not exceed 200 characters').nonempty('Service name is requied'),
+  description: z.string().max(300, 'Description must not exceed 300 characters').nonempty('Description is requied'),
+  price: z.coerce.number('Price is required'),
+  currency: z.enum(['VND', 'USD', 'EUR'], 'Currency is required')
+})
+
+export const LabRequestForm = z.object({
+  service_id: z.coerce.number('Lab service is required.')
+})
