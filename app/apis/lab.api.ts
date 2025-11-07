6176b830704875adef29e9a5592b6d11b6fe1e8d
anhsayshello <anhle0347@gmail.com>
2025-11-07 10:09:08 +0700

    ✨ feat(lab): add lab api endpoints
    - introduce api functions for lab service management
    - enable creation and retrieval of lab services
    - add api functions for lab request management
    - allow creation, retrieval, and cancellation of lab requests
    - provide api functions for lab test management
    - support retrieval of tests by medical id, starting, and finishing tests

---
+++ app/apis/lab.api.ts
@@ -0,0 +1,17 @@
+import type { PaginatedResponse, SearchQueryParams } from '@/types/index.type'
+import { type LabTest, type Service } from '@/types/lab.type'
+import http from '@/utils/http'
+
+const labApi = {
+  createLabService: (body: { service_name: string; description: string; price: number }) =>
+    http.post<Service>('lab/services', body),
+  getLabServices: (params: SearchQueryParams) => http.get<PaginatedResponse<Service>>('lab/services', { params }),
+  createLabRequest: (body: { service_id: number; medical_id: number }) => http.post('lab/requests', body),
+  getLabTestRequests: (params: SearchQueryParams) => http.get<PaginatedResponse<LabTest>>('lab/requests', { params }),
+  getLabTestsByMedicalId: (id: string) => http.get<LabTest[]>(`lab/tests/medical/${id}`),
+  cancelLabRequest: ({ id, notes }: { id: number; notes: string }) => http.post(`lab/requests/${id}/cancel`, { notes }),
+  startLabTest: (id: number) => http.post(`lab/tests/${id}/start`),
+  finishLabTest: ({ id, result }: { id: number; result: string }) => http.post(`lab/tests/${id}/finish`, { result })
+}
+
+export default labApi
