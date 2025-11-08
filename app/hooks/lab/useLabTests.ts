e649efa66c0c51342ad712101a93d25a70bf3b18
anhsayshello <anhle0347@gmail.com>
2025-11-08 15:27:58 +0700

    ✨ feat(lab): add hook to fetch lab tests

--- app/hooks/lab/useLabTests.ts
+++ app/hooks/lab/useLabTests.ts
@@ -2,17 +2,17 @@ import labApi from '@/apis/lab.api'
 import type { SearchQueryParams } from '@/types/index.type'
 import { useQuery } from '@tanstack/react-query'
 
-export default function useLabServices(params: SearchQueryParams) {
+export default function useLabTests(params: SearchQueryParams) {
   const { data, isPending } = useQuery({
-    queryKey: ['lab', 'services', params],
-    queryFn: () => labApi.getLabServices(params)
+    queryKey: ['lab', 'tests', params],
+    queryFn: () => labApi.getLabTests(params)
   })
 
   console.log(data)
-  const dataLabServices = data?.data.data ?? []
+  const dataLabTests = data?.data.data ?? []
   const currentPage = data?.data.currentPage ?? 1
   const totalPages = data?.data.totalPages ?? 0
   const totalRecords = data?.data.totalRecords ?? 0
 
-  return { dataLabServices, currentPage, totalPages, totalRecords, isPending }
+  return { dataLabTests, currentPage, totalPages, totalRecords, isPending }
 }
--- app/hooks/lab/useStartLabTest.ts
+++ app/hooks/lab/useStartLabTest.ts
@@ -1,9 +1,15 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import labApi from '@/apis/lab.api'
 
 export default function useStartLabTest() {
+  const queryClient = useQueryClient()
+
   const { mutate, isPending } = useMutation({
-    mutationFn: labApi.startLabTest
+    mutationFn: labApi.startLabTest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['lab', 'tests'] })
+      queryClient.invalidateQueries({ queryKey: ['lab', 'requests'] })
+    }
   })
 
   return { mutate, isPending }
