fcd59cb931f5df7eed8a28f6f1901bc719d4dca8
anhsayshello <anhle0347@gmail.com>
2025-11-13 08:08:03 +0700

    refactor(appointment): simplify staff role verification

--- app/hooks/appointment/useAppointments.ts
+++ app/hooks/appointment/useAppointments.ts
@@ -1,18 +1,18 @@
 import { keepPreviousData, useQuery } from '@tanstack/react-query'
-import useRole from '../use-role'
+import useRole from '../useRole'
 import { useAuthStore } from '@/stores/useAuthStore'
 import type { AppointmentParams } from '@/types/appointment.type'
 import appointmentApi from '@/apis/appointment.api'
 
 export default function useAppointments(params: AppointmentParams) {
   const user = useAuthStore((state) => state.user)
-  const { isAdmin, isNurse, isLabTechnician } = useRole()
+  const { isStaff } = useRole()
 
   const { data, isPending } = useQuery({
     queryKey: ['appointments', user?.uid, params],
     queryFn: () => appointmentApi.getAppointments(params),
     placeholderData: keepPreviousData,
-    enabled: isAdmin || isNurse || isLabTechnician
+    enabled: isStaff
   })
 
   return { data, isPending }
