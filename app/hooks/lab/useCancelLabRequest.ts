0276589bd3e8ae26eb587d81dc815c7fd1ba2350
anhsayshello <anhle0347@gmail.com>
2025-11-13 08:09:49 +0700

    feat(lab): add query invalidation for lab request cancellation

--- app/hooks/lab/useCancelLabRequest.ts
+++ app/hooks/lab/useCancelLabRequest.ts
@@ -1,9 +1,16 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import labApi from '@/apis/lab.api'
 
 export default function useCancelLabRequest() {
+  const queryClient = useQueryClient()
+
   const { mutate, isPending } = useMutation({
-    mutationFn: labApi.cancelLabRequest
+    mutationFn: labApi.cancelLabRequest,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['medical-record']
+      })
+    }
   })
 
   return { mutate, isPending }
