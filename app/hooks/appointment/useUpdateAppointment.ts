25cfd15f92fe611b296bba44b086319de46e1755
anhsayshello <anhle0347@gmail.com>
2025-11-13 08:09:34 +0700

    refactor(appointment): streamline update hook and cache invalidation

--- app/hooks/appointment/useUpdateAppointment.ts
+++ app/hooks/appointment/useUpdateAppointment.ts
@@ -4,12 +4,12 @@ import { useMutation, useQueryClient } from '@tanstack/react-query'
 export default function useUpdateAppointment() {
   const queryClient = useQueryClient()
   const { mutate, isPending } = useMutation({
-    mutationFn: appointmentApi.updateAppointmentDetail,
+    mutationFn: appointmentApi.updateAppointment,
     onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['appointment'] })
       queryClient.invalidateQueries({ queryKey: ['appointments'] })
       queryClient.invalidateQueries({ queryKey: ['patient', 'appointments'] })
       queryClient.invalidateQueries({ queryKey: ['doctor', 'appointments'] })
-      queryClient.invalidateQueries({ queryKey: ['appointment', 'detail'] })
     }
   })
 
