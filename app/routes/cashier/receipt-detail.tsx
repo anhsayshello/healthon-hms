ef56ec5ce23220ff8a5baddaa2b0f5d80464b01f
anhsayshello <anhle0347@gmail.com>
2025-11-14 23:27:05 +0700

    feat(cashier): implement receipt records and detail page

---
+++ app/routes/cashier/receipt-detail.tsx
@@ -0,0 +1,26 @@
+import type { Route } from './+types/receipt-detail'
+import CardWrapper from '@/components/shared/card-wrapper'
+import { Spinner } from '@/components/ui/spinner'
+import useReceiptById from '@/hooks/cashier/useReceiptById'
+import { useParams } from 'react-router'
+
+import Receipt from '@/components/cashier/receipt'
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: 'Receipt Detail' }, { name: 'description', content: 'Welcome to React Router!' }]
+}
+
+export default function ReceiptDetail() {
+  const { receiptId } = useParams()
+  const { dataReceipt, isPending } = useReceiptById(Number(receiptId))
+
+  if (isPending) {
+    return (
+      <div className='flex items-center justify-center h-full'>
+        <Spinner />
+      </div>
+    )
+  }
+
+  return <CardWrapper className='max-w-4xl mx-auto'>{dataReceipt && <Receipt dataReceipt={dataReceipt} />}</CardWrapper>
+}
---
+++ app/routes/cashier/receipt-records.tsx
@@ -0,0 +1,110 @@
+import type { Route } from './+types/receipt-records'
+import CardWrapper from '@/components/shared/card-wrapper'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+import { Spinner } from '@/components/ui/spinner'
+import useQueryParams from '@/hooks/useQueryParams'
+import TableMetadata from '@/components/shared/table-metadata'
+import AppPagination from '@/components/shared/app-pagination'
+import useReceipts from '@/hooks/cashier/useReceipts'
+import type { Payment } from '@/types/payment.type'
+import UserInfo from '@/components/shared/user-info'
+import { formatDateTime } from '@/helpers/formatDateTime'
+import formatNumber from '@/helpers/formatNumber'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
+import useReceiptById from '@/hooks/cashier/useReceiptById'
+import Receipt from '@/components/cashier/receipt'
+import { useIsMobile } from '@/hooks/use-mobile'
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: 'Receipt List' }, { name: 'description', content: 'Welcome to React Router!' }]
+}
+
+const tableColumns = [
+  { header: 'Invoice', key: 'invoice' },
+  { header: 'Patient', key: 'patient' },
+  { header: 'Date', key: 'date' },
+  { header: 'Cashier', key: 'cashier' },
+  { header: 'Payment Method', key: 'payment-method' },
+  { header: 'Amount', key: 'amount' }
+]
+
+export default function PaymentRecords() {
+  const { query, page, limit, handlePageChange, handleSearch } = useQueryParams()
+  const { dataReceipts, currentPage, totalPages, totalRecords, isPending } = useReceipts({
+    query,
+    page,
+    limit
+  })
+
+  return (
+    <div className='grow h-full flex flex-col gap-4 lg:gap-6 justify-between'>
+      <CardWrapper>
+        <TableMetadata title='Receipt Table' totalRecords={totalRecords} onSearch={handleSearch} />
+        <Table className='bg-background'>
+          <TableHeader>
+            <TableRow>
+              {tableColumns.map((column) => (
+                <TableHead key={column.key}>{column.header}</TableHead>
+              ))}
+            </TableRow>
+          </TableHeader>
+          {dataReceipts && dataReceipts.length === 0 && (
+            <TableCaption className='text-center'>{isPending ? <Spinner /> : 'No data found'}</TableCaption>
+          )}
+          <TableBody>
+            {dataReceipts &&
+              dataReceipts.length > 0 &&
+              dataReceipts.map((receipt) => <ReceiptRow key={receipt.id} receipt={receipt} />)}
+          </TableBody>
+        </Table>
+      </CardWrapper>
+      <AppPagination handlePageChange={handlePageChange} currentPage={currentPage} totalPages={totalPages} />
+    </div>
+  )
+}
+
+function ReceiptRow({ receipt }: { receipt: Payment }) {
+  const { dataReceipt, isPending } = useReceiptById(receipt.id)
+  const isMobile = useIsMobile()
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <TableRow className='cursor-pointer'>
+          <TableCell>{receipt.id}</TableCell>
+          <TableCell>
+            <UserInfo
+              photoUrl={receipt.appointment?.patient.photo_url}
+              firstName={receipt.appointment?.patient.first_name}
+              lastName={receipt.appointment?.patient.last_name ?? ''}
+              description={receipt.appointment?.patient.gender}
+            />
+          </TableCell>
+          <TableCell>{formatDateTime(receipt.payment_date)}</TableCell>
+          <TableCell>
+            <UserInfo
+              photoUrl={receipt.cashier?.photo_url}
+              firstName={receipt.cashier?.first_name}
+              lastName={receipt.cashier?.last_name ?? ''}
+              description={receipt.cashier?.department}
+            />
+          </TableCell>
+          <TableCell>{receipt.payment_method}</TableCell>
+          <TableCell>{formatNumber(receipt.total_amount)}</TableCell>
+        </TableRow>
+      </DialogTrigger>
+      <DialogContent
+        showCloseButton={isMobile}
+        className='w-full md:max-w-3xl max-h-[96vh] md:max-h-[90vh] overflow-y-auto'
+      >
+        {isPending && (
+          <div className='flex items-center justify-center h-[30vh]'>
+            <Spinner />
+          </div>
+        )}
+
+        {dataReceipt && <Receipt dataReceipt={dataReceipt} isDialog />}
+      </DialogContent>
+    </Dialog>
+  )
+}
