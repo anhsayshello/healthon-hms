0a033a7cba9c7207d1e87a2e037217bf8b99b47f
anhsayshello <anhle0347@gmail.com>
2025-11-14 23:22:27 +0700

    feat(doctor): add consultation start/complete APIs

--- app/apis/doctor.api.ts
+++ app/apis/doctor.api.ts
@@ -5,7 +5,11 @@ import type { PaginatedResponse, SearchQueryParams } from '@/types/index.type'
 const doctorApi = {
   getDoctors: (params: SearchQueryParams) => http.get<PaginatedResponse<Doctor>>('doctor', { params }),
   getDoctorDashboardStatistic: () => http.get<DoctorDashboardStatistic>('doctor/statistic'),
-  getDoctorById: (uid: string) => http.get<Doctor>(`doctor/${uid}`)
+  getDoctorById: (uid: string) => http.get<Doctor>(`doctor/${uid}`),
+  startConsultation: (apppointment_id: number) =>
+    http.patch(`doctor/appointments/${apppointment_id}/start_consultation`),
+  completeConsultation: (apppointment_id: number) =>
+    http.patch(`doctor/appointments/${apppointment_id}/complete_consultation`)
 }
 
 export default doctorApi
