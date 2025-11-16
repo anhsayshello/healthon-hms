1ea9ee84e52ee53ec690c35cd269f385bfc4b82c
anhsayshello <anhle0347@gmail.com>
2025-11-16 13:11:15 +0700

    🐛 fix(config): correct base URL

--- app/constants/config.ts
+++ app/constants/config.ts
@@ -1,6 +1,5 @@
 const config = {
-  // baseUrl: 'http://localhost:3000/',
-  baseUrl: 'https://healthon-hms-server.up.railway.app'
+  baseUrl: 'https://healthon-hms-server.up.railway.app/'
 } as const
 
 export default config
