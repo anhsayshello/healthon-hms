17b3a15a83eb103f7641026d0c0818d557ed0efb
anhsayshello <anhle0347@gmail.com>
2025-11-19 17:20:16 +0700

    ♻️ refactor(home): simplify home route

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,56 +1,12 @@
-import path from '@/constants/path'
 import type { Route } from './+types/home'
-import { Link } from 'react-router'
-import Dither from '@/components/Dither'
-import { Button } from '@/components/ui/button'
 import { useAuthStore } from '@/stores/useAuthStore'
 
 export function meta({}: Route.MetaArgs) {
-  return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }]
+  return [{ title: 'Healthon' }, { name: 'description', content: 'Welcome to React Router!' }]
 }
 
 export default function Home() {
   const role = useAuthStore((state) => state.role)
 
-  return (
-    <div className=''>
-      {/* <div className='w-full h-screen relative'>
-        <Dither
-          waveColor={[0.5, 0.5, 0.5]}
-          disableAnimation={false}
-          enableMouseInteraction={true}
-          mouseRadius={0.3}
-          colorNum={4}
-          pixelSize={2}
-          waveAmplitude={0.3}
-          waveFrequency={3}
-          waveSpeed={0.05}
-        />
-        <div className='pointer-events-none'>
-          <div className='absolute top-10 w-full h-15'>
-            <div className='mx-auto w-[90%] md:w-[60%] h-full rounded-full py-4 px-6 flex items-center justify-between text-white bg-white/5 backdrop-blur-[10px] border border-white/20'>
-              <p className='pointer-events-auto'>Healthon</p>
-              <Link to={!role ? path.signIn : path.dashboard} className='pointer-events-auto'>
-                {!role ? 'Get started' : 'Dashboard'}
-              </Link>
-            </div>
-          </div>
-          <div className='absolute inset-0 flex flex-col items-center justify-center'>
-            <Button className='rounded-full bg-white/5 backdrop-blur-[10px] border border-white/20'>Learn</Button>
-            <p className='mt-4 text-3xl md:text-4xl lg:text-[40px] text-center tracking-tighter font-bold text-shadow-lg/20 w-[80%] text-primary-foreground'>
-              Retro healthon waves to enhance your UI
-            </p>
-            <div className='mt-8 flex items-center gap-4'>
-              <button className='pointer-events-auto cursor-pointer py-2 px-6 md:py-3 md:px-10 text-sm xl:text-base rounded-full bg-primary-foreground text-foreground'>
-                Get started
-              </button>
-              <button className='pointer-events-auto cursor-pointer py-2 px-6 md:py-3 md:px-10 text-sm xl:text-base rounded-full bg-white/5 backdrop-blur-[10px] border border-white/20 text-white/30'>
-                Learn more
-              </button>
-            </div>
-          </div>
-        </div>
-      </div> */}
-    </div>
-  )
+  return <div className=''></div>
 }
