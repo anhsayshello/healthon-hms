c1a52b1bcfeb90dbd7063df1724ea9f28c9eac91
anhsayshello <anhle0347@gmail.com>
2025-11-19 17:25:32 +0700

    refactor(home): remove unused auth store import

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,12 +1,11 @@
 import type { Route } from './+types/home'
-import { useAuthStore } from '@/stores/useAuthStore'
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: 'Healthon' }, { name: 'description', content: 'Welcome to React Router!' }]
 }
 
 export default function Home() {
-  const role = useAuthStore((state) => state.role)
+  // const role = useAuthStore((state) => state.role)
 
   return <div className=''></div>
 }
