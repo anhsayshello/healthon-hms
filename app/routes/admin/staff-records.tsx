d95882f2d1e0ac439e93ac41999132d1581cbfd7
anhsayshello <anhle0347@gmail.com>
2025-11-19 17:57:13 +0700

    feat(staff): conditionally render new staff button

--- app/routes/admin/staff-records.tsx
+++ app/routes/admin/staff-records.tsx
@@ -37,7 +37,7 @@ export default function StaffRecords() {
     <div className='grow h-full flex flex-col gap-4 lg:gap-6 justify-between'>
       <CardWrapper>
         <TableMetadata title='Staff Table' totalRecords={totalRecords} onSearch={handleSearch}>
-          <NewStaff />
+          {isAdmin && <NewStaff />}
         </TableMetadata>
         <Table className='bg-background'>
           <TableHeader>
