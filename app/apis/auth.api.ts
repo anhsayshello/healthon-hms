40360572886ebe810ba62bb59c88ac3fb6214cdd
anhsayshello <anhle0347@gmail.com>
2025-11-20 15:57:02 +0700

    ♻️ refactor(auth): simplify verifyUser API call

--- app/apis/auth.api.ts
+++ app/apis/auth.api.ts
@@ -1,8 +1,6 @@
-import type { AuthResponse } from '@/types/auth.type'
-import type { Patient } from '@/types/patient.type'
 import http from '@/utils/http'
 
 const authApi = {
-  verifyUser: (idToken: string) => http.post<AuthResponse<Patient>>('auth', { idToken })
+  verifyUser: () => http.post('auth')
 }
 export default authApi
