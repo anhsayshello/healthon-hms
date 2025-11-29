7de5f7e980d94a67a04a13a0f313a274db086b75
anhsayshello <anhle0347@gmail.com>
2025-11-29 23:05:12 +0700

    feat(auth): integrate firebase user credential

--- app/stores/useUserCredentialStore.ts
+++ app/stores/useUserCredentialStore.ts
@@ -1,20 +1,36 @@
 import { persist } from 'zustand/middleware'
 import { create } from 'zustand'
-import type { GoogleTokenResponse } from '@/types/index.type'
+import type { UserCredential } from 'firebase/auth'
+import type { GoogleTokenResponse } from '@/types/firebase-augment'
 
 type FirebaseUser = Pick<GoogleTokenResponse, 'email' | 'firstName' | 'lastName' | 'photoUrl'>
 
-type UserCredential = {
+type UserCredentialStore = {
   userCred: FirebaseUser | null
-  setUserCred: (userCred: FirebaseUser | null) => void
+  setUserCred: (userCred: UserCredential | null) => void
   clearUserCred: () => void
 }
 
-export const useUserCredential = create<UserCredential>()(
+export const useUserCredentialStore = create<UserCredentialStore>()(
   persist(
     (set) => ({
       userCred: null,
-      setUserCred: (userCred) => set({ userCred: userCred }),
+      setUserCred: (userCred) => {
+        const tokenRes = userCred?._tokenResponse
+
+        if (!tokenRes) {
+          set({ userCred: null })
+          return
+        }
+        set({
+          userCred: {
+            email: tokenRes?.email,
+            firstName: tokenRes?.firstName,
+            lastName: tokenRes?.lastName,
+            photoUrl: tokenRes?.photoUrl
+          }
+        })
+      },
       clearUserCred: () => set({ userCred: null })
     }),
     {
