55aeec8776d30568370c2503387c2e06b792f75e
anhsayshello <anhle0347@gmail.com>
2025-11-29 23:00:45 +0700

    ✨ feat(admin): add get user by id api

--- app/apis/admin.api.ts
+++ app/apis/admin.api.ts
@@ -1,5 +1,6 @@
 import type { Doctor, Weekday } from '@/types/doctor.type'
 import type { FirebaseUserRecord } from '@/types/index.type'
+import { type Patient } from '@/types/patient.type'
 import type { StaffRole } from '@/types/role.type'
 import type { Staff } from '@/types/staff.type'
 import http from '@/utils/http'
@@ -10,6 +11,7 @@ const adminApi = {
   setStaffRole: (body: { uid: string; role: StaffRole }) => http.patch<{ message: string }>('admin/role', body),
   setUserAccess: (body: { uid: string; disabled: boolean }) => http.patch<{ message: string }>('admin/access', body),
   deleteUserById: (uid: string) => http.delete<{ message: string }>(`admin/${uid}`),
+  getUserById: (uid: string) => http.get<Patient | Doctor | Staff>(`admin/user/${uid}`),
   createDoctor: (body: {
     working_days: Weekday[]
     start_time: string
--- app/apis/auth.api.ts
+++ app/apis/auth.api.ts
@@ -1,6 +1,7 @@
+import type UserAuth from '@/types/auth.type'
 import http from '@/utils/http'
 
 const authApi = {
-  verifyUser: () => http.post('auth')
+  verifyUser: () => http.post<UserAuth>('auth')
 }
 export default authApi
