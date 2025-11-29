191aa1ba2e9982d7538bad9f5f5090b5cc7943ac
anhsayshello <anhle0347@gmail.com>
2025-11-29 23:03:49 +0700

    feat(auth): implement authentication store with Firebase

--- app/stores/useAuthStore.ts
+++ app/stores/useAuthStore.ts
@@ -1,48 +1,105 @@
-import { persist } from 'zustand/middleware'
 import { create } from 'zustand'
-import type { Patient } from '@/types/patient.type'
 import type { Role } from '@/types/role.type'
+import { actionCodeSettings, auth } from '@/lib/firebase/client'
+import { onAuthStateChanged, sendEmailVerification, signOut } from 'firebase/auth'
+import type { NavigateFunction } from 'react-router'
+import path from '@/constants/path'
+import authApi from '@/apis/auth.api'
+import { toast } from 'sonner'
+import { FirebaseError } from 'firebase/app'
+import type UserAuth from '@/types/auth.type'
+import { useUserCredentialStore } from './useUserCredentialStore'
 
 type AuthStore = {
   isAuthenticated: boolean
-  user: Patient | null
+  isInitialized: boolean
+  user: UserAuth | null
   role: Role | null
-  setIsAuthenticated: (isAuthenticated: boolean) => void
-  setUser: (user: Patient | null) => void
-  setRole: (role: Role | null) => void
-  clearAuth: () => void
+
+  initializeAuth: (navigate: NavigateFunction) => () => void
+  refreshAuth: () => Promise<void>
+  logOut: () => Promise<void>
 }
 
-export const useAuthStore = create<AuthStore>()(
-  persist(
-    (set) => ({
-      isAuthenticated: false,
-      user: null,
-      role: null,
+export const useAuthStore = create<AuthStore>()((set) => ({
+  isAuthenticated: false,
+  isInitialized: false,
+  user: null,
+  role: null,
 
-      setIsAuthenticated: (isAuthenticated) => {
-        set({
-          isAuthenticated
-        })
-      },
-      setUser: (user) =>
-        set({
-          user
-        }),
-      setRole: (role) =>
-        set({
-          role
-        }),
+  initializeAuth: (navigate) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        if (!user.emailVerified) {
+          try {
+            await sendEmailVerification(user, actionCodeSettings)
+            navigate({ pathname: path.emailVerification })
+          } catch (error) {
+            if (error instanceof FirebaseError) {
+              const errorMessage = error.message
+              toast.error(errorMessage)
+            }
+            console.log(error)
+          }
+          return
+        }
+        try {
+          const [res, idTokenResult] = await Promise.all([authApi.verifyUser(), user.getIdTokenResult()])
+          const userData = res.data
+          const role = idTokenResult.claims?.role as Role
 
-      clearAuth: () =>
+          set({
+            isAuthenticated: true,
+            role,
+            user: userData,
+            isInitialized: true
+          })
+        } catch (error) {
+          console.error(error)
+          set({ isAuthenticated: false, user: null, role: null, isInitialized: true })
+        }
+      } else {
         set({
           isAuthenticated: false,
           user: null,
-          role: null
+          role: null,
+          isInitialized: true
         })
-    }),
-    {
-      name: 'auth-store'
+        useUserCredentialStore.getState().clearUserCred()
+      }
+    })
+
+    return unsubscribe
+  },
+
+  refreshAuth: async () => {
+    const currentUser = auth.currentUser
+    if (!currentUser) return
+
+    try {
+      await currentUser.getIdToken(true)
+      const [res, idTokenResult] = await Promise.all([authApi.verifyUser(), currentUser.getIdTokenResult()])
+      const userData = res.data
+      const role = idTokenResult.claims?.role as Role
+
+      set({
+        isAuthenticated: true,
+        role,
+        user: userData,
+        isInitialized: true
+      })
+    } catch (error) {
+      console.error('Refresh auth failed', error)
     }
-  )
-)
+  },
+
+  logOut: async () => {
+    try {
+      await signOut(auth)
+      set({ isAuthenticated: false, user: null, role: null })
+      useUserCredentialStore.getState().clearUserCred()
+    } catch (error) {
+      console.error('Logout failed:', error)
+    }
+  }
+}))
