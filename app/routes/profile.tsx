a516b12a5912cc273957144ca1501b283cb13f45
anhsayshello <anhle0347@gmail.com>
2025-11-29 23:06:16 +0700

    feat(router): add admin routes for lab services, medication list, and user profile

---
+++ app/routes/profile.tsx
@@ -0,0 +1,29 @@
+import useUserDetail from '@/hooks/admin/useUserDetail'
+import type { Route } from './+types/profile'
+import ProfileCard from '@/components/react-bits/ProfileCard'
+
+export function meta({}: Route.MetaArgs) {
+  return [{ title: 'Profile' }, { name: 'description', content: 'Welcome to React Router!' }]
+}
+
+export default function Profile() {
+  const { data, isPending } = useUserDetail()
+
+  return (
+    <div className='flex items-center justify-center'>
+      <ProfileCard
+        name={`${data?.data.first_name} ${data?.data.last_name}`}
+        title={data?.data.email}
+        handle='javicodes'
+        status='Online'
+        contactText='Contact Me'
+        avatarUrl={data?.data.photo_url as string}
+        showUserInfo={false}
+        enableTilt={false}
+        behindGlowEnabled={false}
+        enableMobileTilt={false}
+        onContactClick={() => console.log('Contact clicked')}
+      />
+    </div>
+  )
+}
